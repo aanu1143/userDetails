0d87a770b8f63fc0e59ad364973094c83feaed4a
Anand Biswas <41153602+aanu1143@users.noreply.github.com>
2020-04-01 22:07:56 +0530

    final commit for today

--- detailapp/views.py
+++ detailapp/views.py
@@ -8,6 +8,6 @@ def index(request):
     user_detail_list = UserDetails.objects.order_by('-id')[:5]
     template = loader.get_template('detailapp/view_web.html')
     context = {
-        'latest_question_list': user_detail_list,
+        'user_detail_list': user_detail_list,
     }
     return HttpResponse(template.render(context, request))
