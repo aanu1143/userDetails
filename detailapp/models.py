e41b8eef7ff05c8874e0594c94695fcee8ef813a
Anand Biswas <41153602+aanu1143@users.noreply.github.com>
2020-04-01 12:56:49 +0530

    insert database

--- detailapp/models.py
+++ detailapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UserDetails(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    contact_number=models.IntegerField()
+    password = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
